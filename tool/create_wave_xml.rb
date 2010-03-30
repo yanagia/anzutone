e57aec954b2352e8067695af357a3b76448379ca
Yanagi Asakura <yanagi.entertainment@gmail.com>
2010-03-31 01:45:02 +0900

    editor, pianoroleからwaveのxmlに変換するスクリプトを書いた。

---
+++ tool/create_wave_xml.rb
@@ -0,0 +1,42 @@
+require "base64"
+
+templete=<<EOF
+<?xml version="1.0" encoding="UTF-8" ?>
+<Module>
+  <ModulePrefs title="Anzutone" height="600">
+    <Require feature="rpc" />
+  </ModulePrefs>
+  <Content type="html">
+    <![CDATA[
+<script type="text/javascript"
+	src="http://wave-api.appspot.com/public/wave.js"></script>
+
+#html_here
+
+    ]]>
+  </Content>
+</Module>
+
+EOF
+
+f = open(ARGV.shift)
+editor = f.read
+f.close
+
+f = open(ARGV.shift)
+piano = f.read
+f.close
+
+publicurl = "http://dl.dropbox.com/u/294534/anzutone/"
+
+editor.gsub!(/src="(.+?)"/, 'src="' + publicurl + '\1' + '"')
+piano.gsub!(/src="(.+?)"/, 'src="' + publicurl + '\1' + '"')
+editor.gsub!(/(css.+?href=")(.+?)"/, '\1' + publicurl + '\2' + '"')
+piano.gsub!(/(css.+?href=")(.+?)"/, '\1' + publicurl + '\2' + '"')
+
+piano_b = 'data:text/html;charset=utf-8;base64,' + Base64.encode64(piano)
+editor.sub!(/(<iframe src=")(.+?)(")/, '\1' + piano_b + '\3')
+
+templete.sub!(/#html_here/, editor)
+
+print templete
